2e812cc3fad5e68b0cebc0c18d37c1a2b7b3ad09
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-09-29 20:41:54 -0300

    Refactor OTLP export configuration in application-prod.properties and update environment variables in Kubernetes deployment for observability settings.

--- terraform/kubernetes-ms-processamento.tf
+++ terraform/kubernetes-ms-processamento.tf
@@ -80,12 +80,12 @@ resource "kubernetes_deployment" "deployment-ms-processamento" {
 
           # Configurações de Observabilidade
           env {
-            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_ENDPOINT"
+            name  = "OTEL_EXPORTER_OTLP_METRICS_ENDPOINT"
             value = "http://otel-collector.observability:4318/v1/metrics"
           }
           env {
-            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_PROTOCOL"
-            value = "http/protobuf"
+            name  = "OTEL_EXPORTER_OTLP_METRICS_ENABLED"
+            value = "true"
           }
           env {
             name  = "MANAGEMENT_METRICS_TAGS_APPLICATION"
