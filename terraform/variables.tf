86db70d9239bbc7b08cae66946b813d404f9c757
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-09-30 18:31:05 -0300

    Update AWS S3 access key variable names in deploy.yml and change default S3 bucket name in variables.tf for consistency and clarity.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 variable "aws_s3_bucket_name" {
   description = "AWS S3 Bucket Name"
   type        = string
-  default     = "bucket-hacka-nic"
+  default     = "bucket-hacka"
 }
 
 variable "aws_s3_access_key_id" {
