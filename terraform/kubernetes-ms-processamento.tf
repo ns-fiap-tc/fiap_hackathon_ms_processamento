0de55661d52205d901c19e47254080108b2d73ec
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-09-30 01:45:35 -0300

    test: adjust props

--- terraform/kubernetes-ms-processamento.tf
+++ terraform/kubernetes-ms-processamento.tf
@@ -78,28 +78,6 @@ resource "kubernetes_deployment" "deployment-ms-processamento" {
             }
           }
 
-          env {
-            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_ENDPOINT"
-            value = "http://otel-collector.observability.svc.cluster.local:4318/v1/metrics"
-          }
-
-          env {
-            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_PROTOCOL"
-            value = "http/protobuf"
-          }
-          env {
-            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_ENABLED"
-            value = "true"
-          }
-          env {
-            name  = "MANAGEMENT_METRICS_TAGS_APPLICATION"
-            value = "processamento-service"
-          }
-          env {
-            name  = "MANAGEMENT_METRICS_TAGS_SERVICE"
-            value = "ms-processamento"
-          }
-
           port {
             container_port = "8080"
           }
